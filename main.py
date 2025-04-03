af85f903e7973741498b5c03f931f23ee9f8c15a
José Miguel Feixas Galdeano <josemiguel.feixas@nucleoo.com>
2025-04-03 13:20:51 +0200

    New features

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-# backend/app.py
+import eventlet
+
+eventlet.monkey_patch()
+
+from flask_cors import CORS
 from flask import Flask, request
 from flask_socketio import SocketIO, send, emit
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para el cliente Angular
+CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
@@ -14,7 +17,24 @@ def index():
 @socketio.on('connect')
 def handle_connect():
     print('Client connected:', request.sid)
-    emit('message', {'user': 'Server', 'text': 'Welcome to the chat!'})
+
+@socketio.on('new_connection')
+def handle_new_connection(data):
+    if data is not None:
+        welcome = f'{data} se ha unido al chat, bienvenido/a! 🤖'
+    else:
+        welcome = 'Alguien se ha unido al chat, bienvenido/a! 🤖'
+
+    emit('message', {'user': 'BipBop el robot', 'text': welcome}, broadcast=True)
+
+@socketio.on('long_chat_message')
+def handle_new_connection(data):
+    if data['user'] is not None:
+        message = f'🚨🚨 Vaya, parece que {data["user"]} ha intentado mandar un mensaje de más de 300 caracteres... Sin duda es un texto bastante largo, no creo que haya copiado un lorem ipsum para intentar romper la página ¿verdad? 🤖'
+    else:
+        message = f'🚨🚨 Vaya, parece que alguien ha intentado mandar un mensaje de más de 300 caracteres... Sin duda es un texto bastante largo, no creo que haya copiado un lorem ipsum para intentar romper la página ¿verdad? 🤖'
+
+    emit('message', {'user': 'BipBop el robot', 'text': message}, broadcast=True)
 
 @socketio.on('disconnect')
 def handle_disconnect():
@@ -22,8 +42,7 @@ def handle_disconnect():
 
 @socketio.on('chat_message')
 def handle_message(data):
-    print('Received message:', data)
-    emit('message', data, broadcast=True)  # broadcast para enviar a todos
+    emit('message', data, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
